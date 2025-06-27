0024dde1ee4f7001ec7b755570b1606c886d9f9d
matheusvilar2019 <43919709+matheusvilar2019@users.noreply.github.com>
2025-06-27 20:26:33 -0300

    cart page MVP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './pages/product/product.component';
 import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'product/:id', component: ProductComponent}
+  {path: 'product/:id', component: ProductComponent},
+  {path: 'cart', component: CartComponent}
 ];
 
 @NgModule({
