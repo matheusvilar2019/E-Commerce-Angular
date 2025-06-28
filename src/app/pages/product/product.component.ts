980bef0711ffa23a4921db39f7251236b4ea489c
matheusvilar2019 <43919709+matheusvilar2019@users.noreply.github.com>
2025-06-27 23:40:02 -0300

    cart cookie and bind data

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -14,11 +15,11 @@ export class ProductComponent {
   productId: string | null = null;
   product: any = null;
 
-  constructor(private route: ActivatedRoute, private productService: ProductService) {
-    this.productId = this.route.snapshot.paramMap.get('id');    
+  constructor(private route: ActivatedRoute, private productService: ProductService, private cookieService: CookieService, private router: Router) {
+    this.productId = this.route.snapshot.paramMap.get('id');
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.productService.getProduct(this.productId).subscribe(
       (data) => {
         this.product = data.data;
@@ -28,5 +29,54 @@ export class ProductComponent {
         console.error('Erro ao obter dados sobre o produto', error);
       }
     );
+
+    this.lerCarrinho();
+  }
+
+  salvarNoCarrinho() {
+    const cookie = this.cookieService.get('carrinho');
+    var cart = cookie ? JSON.parse(cookie) : [];
+    const addProduct = {
+      id: this.product.id,
+      name: this.product.name,
+      price: this.product.price,
+      description: this.product.description,
+      slug: this.product.slug,
+      image: this.product.image,
+      quantidade: 1
+    };
+
+    if (!cart || cart.length === 0) {
+      cart = [addProduct];
+    }
+    else {
+      var addnew: Boolean = true;
+      // Is this product already is in the array?
+      cart.forEach((x: { id: any; quantidade: number; }) => {
+        if (x.id == addProduct.id) {
+          x.quantidade++;
+          addnew = false;
+        } 
+      });
+      if (addnew) cart.push(addProduct);
+    }
+
+    this.cookieService.set('carrinho', JSON.stringify(cart), {
+      expires: 7, // dias
+      path: '/',
+      sameSite: 'Lax'
+    });
+
+    this.router.navigate(['/cart'])
+  }
+
+  lerCarrinho() {
+    const cookie = this.cookieService.get('carrinho');
+    const carrinho = cookie ? JSON.parse(cookie) : [];
+    console.log(carrinho);
+  }
+
+  limparCarrinho() {
+    this.cookieService.delete('carrinho', '/');
   }
 }
